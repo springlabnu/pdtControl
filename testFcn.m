d9b47f2aefb24b5053fb73247d16ffda17e2eaea
kercheric <53018140+kercheric@users.noreply.github.com>
2021-04-30 10:53:04 -0400

    Create testFcn.m

---
+++ testFcn.m
@@ -0,0 +1,18 @@
+function testFcn(obj, ~, d, duty)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+output = 10*ones(d.Rate, 1);
+output(round(d.Rate*duty):end) = 0;
+
+data = readwrite(d, output);
+
+t0 = obj.TasksExecuted*obj.Period;
+
+time = seconds(data.Time) + t0;
+
+hold on;
+plot(time, data.Variables);
+
+end
+
